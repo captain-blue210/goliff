77f2e04ef2b2d1988a6475cc674268a39b879146
captain-blue210 <captain.blue210@gmail.com>
2020-07-20 22:53:39 +0900

    feat:OpenWeatherAPIからJSON取得する処理を実装

--- web/api/fetch_weather_data.go
+++ web/api/fetch_weather_data.go
@@ -2,11 +2,21 @@ package api
 
 import (
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
+	"os"
 )
 
 func FetchWeatherData() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.JSON(fasthttp.StatusOK, "Weather Data")
+		key := os.Getenv("OPEN_WEATHER_API_KEY")
+		url := "https://api.openweathermap.org/data/2.5/weather?id=1850144&appid=" + key
+
+		cd, body, err := fasthttp.Get(nil, url)
+		if err != nil && cd != fasthttp.StatusOK {
+			logrus.Fatalf("Error get weather data %v", err)
+		}
+
+		return c.JSON(fasthttp.StatusOK, string(body))
 	}
 }
